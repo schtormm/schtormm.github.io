ca27b893b64df918fd41c66477742ce4c654d8ba
schtormm <storm.noordermeer@gmail.com>
2025-03-18 22:34:56 +0100

    modify text

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
                 <p className="mt-2">
                   {" "}
                   I like racing games and tech (anything from open source
-                  software to weird little gadgets).
+                  software to old consoles).
                 </p>
               </div>
             </div>
